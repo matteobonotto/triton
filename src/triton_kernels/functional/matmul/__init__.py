d621e181f9ffa6a96bcb941c7d948e37fa94ffce
matteobonotto <matteob.90@hotmail.it>
2025-11-20 16:48:29 +0000

    marmul module refactoring

---
+++ src/triton_kernels/functional/matmul/__init__.py
@@ -0,0 +1,9 @@
+from .eager import matmul as eager_matmul
+from .persistent import matmul as persistent_matmul
+from .swizzle import matmul as swizzle_matmul
+
+__all__ = [
+    "eager_matmul", 
+    "persistent_matmul",
+    "swizzle_matmul",
+]
